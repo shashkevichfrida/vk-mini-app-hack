203369fbdf845af29b28bdc2ca709f246d8b01dc
Elfrida Shashkevich <elfrida.shashkevich@gmail.com>
2024-10-12 09:35:48 +0300

    add student module

---
+++ src/student/entity/student.ts
@@ -0,0 +1,20 @@
+
+
+
+export class Student {
+    id: string;
+
+    faculty: string;
+
+    level: string
+
+    course: number
+
+    sport: string
+
+    club: string
+
+    event: string
+
+    money: number
+}
